381166cc65bb01a0690a9f3e5b8b6fad149c3879
Seshien <maciek22999@wp.pl>
2016-08-19 23:52:02 +0200

    Add files via upload

---
+++ EM_defines.lua
@@ -0,0 +1,4 @@
+NDefines.NGameplay.NAVY_SIZE_MAX = 2000	-- Max value of Naval Capacity
+NDefines.NGameplay.NAVY_SIZE_BASE = 10	-- Base value of Naval Capacity
+
+NDefines.NAI.NAVY_BUDGET_FRACTION = 0.35 			-- AI will spend this fraction of their income on ships
